bab4fcdad021d6279421436aa9e0111fab0eff7a
ori0duck <ramifriend6@gmail.com>
2025-05-12 13:30:41 +0900

    처음 커밋

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeroVideo from './components/HeroVideo';
+import ProductPage from './components/ProductPage'; // ⬅️ ProductPage 컴포넌트도 import
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeroVideo />} />
+        <Route path="/product" element={<ProductPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
