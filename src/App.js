157653122181c6c65d539de89f2adb57076f078e
mosoy2 <soyeon2427@gmail.com>
2025-05-27 19:31:11 +0900

    Community

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroVideo from './components/HeroVideo';
-import ProductPage from './components/ProductPage'; // ⬅️ ProductPage 컴포넌트도 import
+import ProductPage from './components/ProductPage';
 import QuestionPage from './components/QuestionPage';
 import ResultPage from './components/ResultPage';
-import GetYourChocoPage from './components/GetYourChocoPage'; 
-import LoginPage from './components/LoginPage'; // ✅ 로그인 페이지 import 추가
+import GetYourChocoPage from './components/GetYourChocoPage';
+import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
-import OurStory from './components/OurStory'; // 파일 위치가 ./components라면 이렇게
-
+import OurStory from './components/OurStory';
+import Community from './components/Community'; // ✅ 커뮤니티 페이지 import 추가
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 메인 화면 */}
         <Route path="/" element={<HeroVideo />} />
+        
+        {/* 상품 페이지 */}
         <Route path="/product" element={<ProductPage />} />
-        {/* Get Your Choco 화면 */}
+
+        {/* 초코 성격 테스트 페이지 */}
         <Route path="/getyourchoco" element={<GetYourChocoPage />} />
-        {/* 테스트 시작 화면 */}
         <Route path="/start" element={<QuestionPage />} />
-        {/* 테스트 결과 화면 */}
         <Route path="/result" element={<ResultPage />} />
-        <Route path="/login" element={<LoginPage />} /> {/* ✅ 로그인 경로 추가 */}
+
+        {/* 회원 관련 페이지 */}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
+        {/* 브랜드 소개 */}
         <Route path="/brand" element={<OurStory />} />
 
+        {/* ✅ 커뮤니티 페이지 경로 추가 */}
+        <Route path="/community" element={<Community />} />
       </Routes>
     </Router>
   );
