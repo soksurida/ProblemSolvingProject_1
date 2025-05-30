2ef3195b8128ac5f9501e4edde48a2f93d94efcc
mosoy2 <soyeon2427@gmail.com>
2025-05-30 15:50:57 +0900

    Getyourchoco, Header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroVideo from './components/HeroVideo';
 import ProductPage from './components/ProductPage';
-import QuestionPage from './components/QuestionPage';
-import ResultPage from './components/ResultPage';
 import GetYourChocoPage from './components/GetYourChocoPage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
@@ -18,14 +16,10 @@ function App() {
         <Route path="/" element={<HeroVideo />} />
         <Route path="/product" element={<ProductPage />} />
         <Route path="/getyourchoco" element={<GetYourChocoPage />} />
-        <Route path="/start" element={<QuestionPage />} />
-        <Route path="/result" element={<ResultPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/brand" element={<OurStory />} />
         <Route path="/cart" element={<CartPage />} />
-
-        {/* ✅ 커뮤니티 페이지 경로 추가 */}
         <Route path="/community" element={<Community />} />
       </Routes>
     </Router>
