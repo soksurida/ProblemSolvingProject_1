74f27c4c9a255061166a14673ea68b096c404bbd
soksurida <ase3908@gmail.com>
2025-06-01 03:00:07 +0900

    결제창 임시 주소 반영

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeroVideo from './components/HeroVideo';
-import ProductPage from './components/ProductPage';
-import ProductDetail from './components/ProductDetail'; // ✅ 상세페이지 컴포넌트 추가
-import GetYourChocoPage from './components/GetYourChocoPage';
-import LoginPage from './components/LoginPage';
-import SignupPage from './components/SignupPage';
 import CartPage from './components/CartPage';
-import OurStory from './components/OurStory';
 import Community from './components/Community';
+import GetYourChocoPage from './components/GetYourChocoPage';
+import HeroVideo from './components/HeroVideo';
+import LoginPage from './components/LoginPage';
+import OurStory from './components/OurStory';
+import PaymentPage from './components/PaymentPage'; //임시 결제창 페이지
+import ProductDetail from './components/ProductDetail'; // ✅ 상세페이지 컴포넌트 추가
+import ProductPage from './components/ProductPage';
+import SignupPage from './components/SignupPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HeroVideo />} />
-        <Route path="/product" element={<ProductPage />} />
-        <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ 상세페이지 라우팅 추가 */}
-        <Route path="/getyourchoco" element={<GetYourChocoPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/brand" element={<OurStory />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/community" element={<Community />} />
+        <Route path="/getyourchoco" element={<GetYourChocoPage />} />
+        <Route path="/" element={<HeroVideo />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/brand" element={<OurStory />} />
+        <Route path="/payment" element={<PaymentPage />} /> {/*임시 결제창 페이지 주소*/}
+        <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ 상세페이지 라우팅 추가 */}
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
       </Routes>
     </Router>
   );
